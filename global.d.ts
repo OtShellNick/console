66ecd712516d42a9b78209e29378bdafbd7e4c9e
Aleksandr <exte92@gmail.com>
2022-05-24 14:01:34 +0300

    up project config

---
+++ global.d.ts
@@ -0,0 +1,8 @@
+declare module '@';
+declare module '@/*';
+declare module '@components';
+declare module '@components/*';
+declare module '*.svg?tsx' {
+    const content: any;
+    export default content;
+}
